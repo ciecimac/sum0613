6fa40541ce3fb54b3b6364796881b7ba2687b8ac
ciecimac <ciecclm@imnu.edu.cn>
2017-06-13 10:48:12 +0800

    Initial Commit

---
+++ sum0613/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sum0613
+//
+//  Created by clm on 2017/6/13.
+//  Copyright © 2017年 clm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
