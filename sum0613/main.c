79e54b4aced221941bf959d6e967b8e2ea883f46
ciecimac <ciecclm@imnu.edu.cn>
2017-06-13 10:51:17 +0800

    测试文件读写
    测试

--- sum0613/main.c
+++ sum0613/main.c
@@ -6,10 +6,19 @@
 //  Copyright © 2017年 clm. All rights reserved.
 //
 
+
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    FILE *fr;
+    FILE *fw;
+    fr=fopen("//Users//s2016110119//Desktop//sum0613//input.txt","r+");
+    fw=fopen("//Users//s2016110119//Desktop//sum0613//output.txt","w");
+    int a,b,c;
+    fscanf(fr,"%d%d",&a,&b);
+    c=a+b;
+    printf("%d+%d=%d\n",a,b,c);
+    fprintf(fw,"%d+%d=%d\n",a,b,c);
     return 0;
 }
